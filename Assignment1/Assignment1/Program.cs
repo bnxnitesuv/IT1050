b2de745236443d64ae127afd879871593ddcfe63
bnxnitesuv <teresajsohg@outlook.com>
2015-09-15 21:29:54 -0400

    Assignment 1

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            userName = "teresa";
+            string middleInitial;
+            middleInitial = "D";
+            string lastName;
+            lastName = "Jones";
+            string fullName;
+            fullName = "Teresa M.Jones";
+
+            int heightFeet;
+            heightFeet = 5;
+            int heightInches;
+            heightInches = 1.50;
+            int totalHeightCM;
+            totalHeightCM = 173;
+            int age;
+            age = 53;
+
+            bool isCitizen;
+            isCitizen = true;
+            canVote;
+
+            canVote = true;
+
+
+
+            Age "53";
+            int heightFeet;
+            heightFeet = "5'8";
+
+            bool isCitizen;
+            bool canVote;
+        }
+    }
+}
